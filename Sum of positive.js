81f90ab4f72edba1848cf2a01d9ccc5053980027
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-18 00:46:32 +0430

    Sum of positive.js added

---
+++ Sum of positive.js
@@ -0,0 +1,14 @@
+/* -------------------------------------------------------------------------- */
+/*                               Sum of positive                              */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/5715eaedb436cf5606000381
+/* You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
+
+function positiveSum(arr) {
+	return arr.filter((element) => element > 0).reduce((value1, value2) => value1 + value2, 0);
+}
