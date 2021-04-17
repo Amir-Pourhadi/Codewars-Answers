4d170f5c79738d13971e0a7e294d20ba8afcc77c
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-17 22:31:54 +0430

    Even or Odd.js added

---
+++ Even or Odd.js
@@ -0,0 +1,11 @@
+/* -------------------------------------------------------------------------- */
+/*                                 Even or Odd                                */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+/* Create a function that takes an integer as an argument
+and returns "Even" for even numbers or "Odd" for odd numbers.*/
+
+function even_or_odd(number) {
+	return number % 2 ? "Odd" : "Even";
+}
