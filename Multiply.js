719ee64024b9dc78080290cbe048efdda6e02743
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-17 21:58:44 +0430

    multiply.js added

---
+++ Multiply.js
@@ -0,0 +1,10 @@
+/* -------------------------------------------------------------------------- */
+/*                                  Multiply                                  */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/50654ddff44f800200000004
+// A function that outputs multiply of two numbers
+
+function multiply(a, b) {
+	a * b;
+}
