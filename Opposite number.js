4fe329dcf7ec0b8c3aa2724cf62cefe8d85246f7
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-17 22:52:45 +0430

    Opposite number.js added

---
+++ Opposite number.js
@@ -0,0 +1,14 @@
+/* -------------------------------------------------------------------------- */
+/*                               Opposite number                              */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+/* Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 */
+
+const opposite = (number) => -number;
