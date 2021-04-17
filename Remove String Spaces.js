70dcdb466266a44eb4c95fd81362f1f833ee212e
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-18 00:06:50 +0430

    Remove String Spaces.js added

---
+++ Remove String Spaces.js
@@ -0,0 +1,10 @@
+/* -------------------------------------------------------------------------- */
+/*                            Remove String Spaces                            */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+/* Simple, remove the spaces from the string, then return the resultant string. */
+
+function noSpace(string) {
+	return string.split(" ").join("");
+}
