74fc52f5462b9dd70f82f6cb43b8d30cec508230
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-19 22:29:10 +0430

    #3.js added

---
+++ JavaScript Kata/#3.js
@@ -0,0 +1,47 @@
+/* -------------------------------------------------------------------------- */
+/*                          Basic data types--String                          */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/571edea4b625edcb51000d8e
+/* Mission 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them. (Please note the uppercase and lowercase letters are different).
+
+Mission 2: After mission 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3 */
+
+let a1 = "A",
+	a2 = "a",
+	b1 = "B",
+	b2 = "b",
+	c1 = "C",
+	c2 = "c",
+	d1 = "D",
+	d2 = "d",
+	e1 = "E",
+	e2 = "e",
+	n1 = "N",
+	n2 = "n";
+function Dad() {
+	//select some variable to combine "Dad"
+	return d1 + a2 + d2;
+}
+function Bee() {
+	//select some variable to combine "Bee"
+	return b1 + e2 + e2;
+}
+function banana() {
+	//select some variable to combine "banana"
+	return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+//answer some questions if you finished works above
+function answer1() {
+	//the answer should be "yes" or "no"
+	return "no";
+}
+function answer2() {
+	//the answer should be "yes" or "no"
+	return "no";
+}
+function answer3() {
+	//the answer should be "yes" or "no"
+	return "yes";
+}
