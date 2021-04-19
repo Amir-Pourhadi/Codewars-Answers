3edb2e2913382efe696e7222395a0a03698425bf
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-19 23:30:05 +0430

    #6.js added

---
+++ JavaScript Kata/#6.js
@@ -0,0 +1,10 @@
+/* -------------------------------------------------------------------------- */
+/*                  Basic data types--Boolean and conditional                 */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/571f832f07363d295d001ba8
+/* Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true". */
+
+function trueOrFalse(val) {
+	return val ? "true" : "false";
+}
