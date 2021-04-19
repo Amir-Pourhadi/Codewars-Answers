50f01200926d67fe848c69cb3ceb65e792ce952c
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-19 22:11:06 +0430

    #1.js added

---
+++ JavaScript Kata/#1.js
@@ -0,0 +1,18 @@
+/* -------------------------------------------------------------------------- */
+/*            create your first JS function and print "Hello World!"           */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+/* mission 1:
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+type the console.log() in the next line(don't forget to put the str in the brackets). */
+
+function helloWorld() {
+	let str = "Hello World!";
+	console.log(str);
+}
