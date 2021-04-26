9bbffba1b16978f4a8b6fc8b147daaffe762776a
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-27 01:18:17 +0430

    Split The Bill.js added

---
+++ JavaScript Kata/Split The Bill.js
@@ -0,0 +1,33 @@
+/* -------------------------------------------------------------------------- */
+/*                               Split The Bill                               */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/5641275f07335295f10000d0
+/* It's tricky keeping track of who is owed what when spending money in a group.
+Write a function to balance the books.
+
+The function should take one parameter: an object/dict with two or more name-value pairs which represent the members of the group and the amount spent by each.
+The function should return an object/dict with the same names, showing how much money the members should pay or receive.
+Further points:
+
+The values should be positive numbers if the person should receive money from the group, negative numbers if they owe money to the group.
+If value is a decimal, round to two decimal places.
+
+Example
+3 friends go out together: A spends £20, B spends £15, and C spends £10. The function should return an object/dict showing that A should receive £5, B should receive £0, and C should pay £5.
+
+var group = {
+    A: 20, 
+    B: 15, 
+    C: 10
+}
+
+splitTheBill(group) // returns {A: 5, B: 0, C: -5} */
+
+function splitTheBill(group) {
+	const average = Object.values(group).reduce((acc, val) => acc + val) / Object.values(group).length;
+	for (const key in group) {
+		group[key] = Math.round((group[key] - average) * 100) / 100;
+	}
+	return group;
+}
