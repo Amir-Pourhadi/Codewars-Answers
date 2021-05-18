cd56d166581f91f2c2039806ab070df3b50c1e66
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-18 05:45:44 +0430

    Rot13.js added

---
+++ JavaScript Kata/Rot13.js
@@ -0,0 +1,16 @@
+/* -------------------------------------------------------------------------- */
+/*                                    Rot13                                   */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/530e15517bc88ac656000716
+/* ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13.
+If there are numbers or special characters included in the string, they should be returned as they are.
+Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation". */
+
+function rot13(message) {
+	return message.replace(/[a-z]/gi, (str) =>
+		String.fromCharCode(str.charCodeAt(0) + (str.toLowerCase() < "n" ? 13 : -13))
+	);
+}
