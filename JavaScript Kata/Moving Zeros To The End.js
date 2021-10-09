84d9eb299af6f5a7de0afe330d7759ad05f13377
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-09 18:05:05 +0330

    Empty collections should not be  iterated

--- JavaScript Kata/Moving Zeros To The End.js
+++ JavaScript Kata/Moving Zeros To The End.js
@@ -8,9 +8,5 @@
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0] */
 
 const moveZeros = (arr) => {
-	const newArr = [];
-	return newArr.concat(
-		arr.filter((element) => element !== 0),
-		arr.filter((element) => element === 0)
-	);
+  return arr.filter((element) => element !== 0).concat(arr.filter((element) => element === 0));
 };
