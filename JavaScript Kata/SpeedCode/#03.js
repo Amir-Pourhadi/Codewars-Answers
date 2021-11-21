fec343acd6b27826dc26904c2652995c43ab2364
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-21 22:28:14 +0330

    Do SpeedCode #03

---
+++ JavaScript Kata/SpeedCode/#03.js
@@ -0,0 +1,29 @@
+/* -------------------------------------------------------------------------- */
+/*                              Dogs and Classes                              */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/56ff9b53140fcca90b000530
+/* 
+Objective
+Preloaded for you is a class Dog:
+
+class Dog {
+  constructor(name, age, gender, species, size, master, loyal) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = species;
+    this.legs = 4;
+    this.size = size;
+    this.master = master;
+    this.loyal = loyal;
+  }
+}
+
+You are then given a working class Labrador as your initial code.
+
+Shorten it so that it meets the strict character count requirements for this Kata.
+Quick, get your timer out and get ready to time yourself. Are you ready? Ready, get set, GO!!! :D */
+
+//! Question and Solution is same as #05.js in Fun with ES6 Classes folder!
+//? Direct link to solution => https://github.com/Amir-Pourhadi/Codewars-Answers/blob/main/JavaScript%20Kata/Fun%20with%20ES6%20Classes/%2305.js
