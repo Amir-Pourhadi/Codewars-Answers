b2b63ff69eaae796c21f3c933d8b8320ff260179
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-11-28 08:10:49 +0330

    Add #1 Beginner Series

---
+++ JavaScript Kata/Beginner Series/#01.js
@@ -0,0 +1,17 @@
+/* -------------------------------------------------------------------------- */
+/*                              School Paperwork                              */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+/* Your classmates asked you to copy some paperwork for them.
+You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+Example:
+n= 5, m=5: 25
+n=-5, m=5:  0 */
+
+function paperwork(n, m) {
+  return n < 0 || m < 0 ? 0 : n * m;
+}
