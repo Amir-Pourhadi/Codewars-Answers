2450b76578e3357dcc57377be72d660d9ecac4a8
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-10 11:56:50 +0330

    Do #3 beginners series

---
+++ JavaScript Kata/Beginner Series/#03.js
@@ -0,0 +1,22 @@
+/* -------------------------------------------------------------------------- */
+/*                               Sum of Numbers                               */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/55f2b110f61eb01779000053
+/* Given two integers a and b, which can be positive or negative,
+find the sum of all the integers between and including them and return it.
+If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2) */
+
+function getSum(a, b) {
+  return ((Math.abs(a - b) + 1) * (a + b)) / 2;
+}
