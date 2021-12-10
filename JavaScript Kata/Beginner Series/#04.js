50e6e053aeca8dd6e657d2eaa62a63d6ee55f189
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-10 12:10:08 +0330

    Do #04 of beginner series

---
+++ JavaScript Kata/Beginner Series/#04.js
@@ -0,0 +1,20 @@
+/* -------------------------------------------------------------------------- */
+/*                                  Cockroach                                 */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+/* The cockroach is one of the fastest insects.
+Write a function which takes its speed in km per hour and returns it in cm per second,
+rounded down to the integer (= floored).
+
+For example:
+1.08 --> 30
+2.49 --> 83
+3.20 --> 88
+
+Note! The input is a Real number (actual type is language dependent) and is >= 0.
+The result should be an Integer. */
+
+function cockroachSpeed(s) {
+  return Math.floor((s * 100000) / 3600);
+}
