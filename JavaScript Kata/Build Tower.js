2371733312e1e5dd001b976dd52de84348f04557
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-09-19 22:27:58 +0430

    Do Build Tower.js

---
+++ JavaScript Kata/Build Tower.js
@@ -0,0 +1,32 @@
+/* -------------------------------------------------------------------------- */
+/*                                 Build Tower                                */
+/* -------------------------------------------------------------------------- */
+
+// https://www.codewars.com/kata/576757b1df89ecf5bd00073b
+/* Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+] */
+
+function towerBuilder(nFloors) {
+  const tower = [];
+  for (let i = nFloors; i > 0; i--)
+    tower.push(" ".repeat(nFloors - i) + "*".repeat(2 * i - 1) + " ".repeat(nFloors - i));
+
+  return tower;
+}
