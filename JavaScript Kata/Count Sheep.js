d8d705eb1b60e80b5990b0536dd74d275b3b7904
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-09-24 17:36:54 +0330

    Do Count Sheep.js

---
+++ JavaScript Kata/Count Sheep.js
@@ -0,0 +1,14 @@
+/* -------------------------------------------------------------------------- */
+/*                   If you can't sleep, just count sheep!!                   */
+/* -------------------------------------------------------------------------- */
+// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+/* If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
+
+function countSheep(num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) str += `${i} sheep...`;
+  return str;
+}
